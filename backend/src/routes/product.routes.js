e794de0f7b2681ebd0afa11d5e2c9ed7edfd6bf2
Felipe Rojas <pradofeliperojas@gmail.com>
2025-03-13 18:41:33 -0500

    Refactor

--- backend/src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Ruta de prueba
-router.get("/", (req, res) => {
-  res.json({ message: "Product route working!" });
-});
-
-module.exports = router;
