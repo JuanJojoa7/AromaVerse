1957bf1e59eae70200ce39d0d92f44b003f02714
juanex2001 <114438748+juanex2001@users.noreply.github.com>
2025-03-23 16:12:36 -0500

    fix: Fixed container controller test
    This commit creates a testServer so request from supertest works. Still needs to fix auth.

--- backend/test/container.controller.test.ts
+++ backend/test/container.controller.test.ts
@@ -1,11 +1,10 @@
-import request from 'supertest';
-import express from 'express';
 import app from '../index.ts'; // Import the app instance from index.js
-import {describe, expect, test, beforeEach} from '@jest/globals';
-import { ContainerService } from '../src/services';
-import { jest } from '@jest/globals';
+import {describe, expect, test} from '@jest/globals';
 
-const containerService = new ContainerService();
+import testServer from '../src/utils/testServer.ts';
+
+const request = testServer(app);
+
 
 describe('POST /products/container', () => {
     const validContainer = {
@@ -15,14 +14,8 @@ describe('POST /products/container', () => {
     };
 
     test('should respond with a 201 status code', async () => {
-        const response = await request(app).post('/products/container').send(validContainer);
+        const response = await request.post('/products/container').send(validContainer);
         expect(response.status).toBe(201);
-    });
-
-    test('should respond with ', async () => {
-        const response = await request(app).get('/products/container');
-        expect(response.status).toBe(200);
-    
     });
 });
 
