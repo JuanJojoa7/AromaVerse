a9e6074fc8c8ff44fb2d20896457a47e8d3053f5
rojas435 <pradofeliperojas@gmail.com>
2025-03-24 22:43:21 -0500

    Update Schema parameters

--- backend/src/schemas/user.schemas.ts
+++ backend/src/schemas/user.schemas.ts
@@ -4,7 +4,7 @@ export const userSchema = object({
     name: string({required_error: 'Name is required'}).min(1, {message: 'Name cannot be empty'}),
     email: string({required_error: 'Email is required'}).min(1, {message: 'Email cannot be empty'}),
     password: string({required_error: 'Password is required'})
-        .min(8, {message: 'Password must be at least 8 characters long'}),
+        .min(5, {message: 'Password must be at least 5 characters long'}),
     phone: string({required_error: 'Number is required'}).min(10, {message: 'Number must be at least 10 characters long'}),
     address: string({required_error: 'Address is required'}).min(1, {message: 'Address cannot be empty'})
 
